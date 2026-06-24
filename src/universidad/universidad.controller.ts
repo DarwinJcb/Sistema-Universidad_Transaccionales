f6532e47c6bba24c95c545c17c7f51d347679220
Dark <djcabrera@sudamericano.edu.ec>
2026-06-24 06:10:33 -0500

    Commit #24

--- src/universidad/universidad.controller.ts
+++ src/universidad/universidad.controller.ts
@@ -1,60 +1,51 @@
 /* src/universidad/universidad.controller.ts: */
 import { Body, Controller, Post } from '@nestjs/common';
-import {
-  CrearPeticion1Dto,
-  RespuestaPeticion1,
-} from './dto/crear-peticion-1.dto';
-import {
-  CrearPeticion2Dto,
-  RespuestaPeticion2,
-} from './dto/crear-peticion-2.dto';
-import {
-  CrearPeticion3Dto,
-  RespuestaPeticion3,
-} from './dto/crear-peticion-3.dto';
+import { CrearPeticion1Dto, RespuestaPeticion1, } from './dto/crear-peticion-1.dto';
+import { CrearPeticion2Dto, RespuestaPeticion2, } from './dto/crear-peticion-2.dto';
+import { CrearPeticion3Dto, RespuestaPeticion3 } from './dto/crear-peticion-3.dto';
 import { UniversidadService } from './universidad.service';
 
 @Controller('universidad')
 export class UniversidadController {
-  constructor(private readonly universidadService: UniversidadService) {}
+    constructor(private readonly universidadService: UniversidadService) { }
 
-  @Post('peticion-1')
-  async crearPeticion1(
-    @Body() datosPeticion: CrearPeticion1Dto,
-  ): Promise<RespuestaPeticion1> {
-    const { Carrera, Materias } = datosPeticion;
+    @Post('peticion-1')
+    async crearPeticion1(
+        @Body() datosPeticion: CrearPeticion1Dto,
+    ): Promise<RespuestaPeticion1> {
+        const { Carrera, Materias } = datosPeticion;
 
-    return await this.universidadService.crearPeticion1({
-      Carrera,
-      Materias,
-    });
-  }
+        return await this.universidadService.crearPeticion1({
+            Carrera,
+            Materias,
+        });
+    }
 
-  @Post('peticion-2')
-  async crearPeticion2(
-    @Body() datosPeticion: CrearPeticion2Dto,
-  ): Promise<RespuestaPeticion2> {
-    const { Ciclo, Carrera, Estudiantes, Matricula } = datosPeticion;
+    @Post('peticion-2')
+    async crearPeticion2(
+        @Body() datosPeticion: CrearPeticion2Dto,
+    ): Promise<RespuestaPeticion2> {
+        const { Ciclo, Carrera, Estudiantes, Matricula } = datosPeticion;
 
-    return await this.universidadService.crearPeticion2({
-      Ciclo,
-      Carrera,
-      Estudiantes,
-      Matricula,
-    });
-  }
+        return await this.universidadService.crearPeticion2({
+            Ciclo,
+            Carrera,
+            Estudiantes,
+            Matricula,
+        });
+    }
 
-  @Post('peticion-3')
-  async crearPeticion3(
-    @Body() datosPeticion: CrearPeticion3Dto,
-  ): Promise<RespuestaPeticion3> {
-    const { Laboratorio, Ciclo, Matricula, Materia } = datosPeticion;
+    @Post('peticion-3')
+    async crearPeticion3(
+        @Body() datosPeticion: CrearPeticion3Dto,
+    ): Promise<RespuestaPeticion3> {
+        const { Laboratorio, Ciclo, Matricula, Materia } = datosPeticion;
 
-    return await this.universidadService.crearPeticion3({
-      Laboratorio,
-      Ciclo,
-      Matricula,
-      Materia,
-    });
-  }
+        return await this.universidadService.crearPeticion3({
+            Laboratorio,
+            Ciclo,
+            Matricula,
+            Materia,
+        });
+    }
 }
--- src/universidad/universidad.service.ts
+++ src/universidad/universidad.service.ts
@@ -1,56 +1,47 @@
 /* src/universidad/universidad.service.ts: */
 import { Injectable } from '@nestjs/common';
-import {
-  CrearPeticion1Dto,
-  RespuestaPeticion1,
-} from './dto/crear-peticion-1.dto';
-import {
-  CrearPeticion2Dto,
-  RespuestaPeticion2,
-} from './dto/crear-peticion-2.dto';
-import {
-  CrearPeticion3Dto,
-  RespuestaPeticion3,
-} from './dto/crear-peticion-3.dto';
+import { CrearPeticion1Dto, RespuestaPeticion1, } from './dto/crear-peticion-1.dto';
+import { CrearPeticion2Dto, RespuestaPeticion2, } from './dto/crear-peticion-2.dto';
+import { CrearPeticion3Dto, RespuestaPeticion3, } from './dto/crear-peticion-3.dto';
 import { UniversidadRepository } from './universidad.repository';
 
 @Injectable()
 export class UniversidadService {
-  constructor(private readonly universidadRepository: UniversidadRepository) {}
+    constructor(private readonly universidadRepository: UniversidadRepository) { }
 
-  async crearPeticion1(
-    datosPeticion: CrearPeticion1Dto,
-  ): Promise<RespuestaPeticion1> {
-    const { Carrera, Materias } = datosPeticion;
-    return await this.universidadRepository.crearCarreraConMaterias({
-      Carrera,
-      Materias,
-    });
-  }
+    async crearPeticion1(
+        datosPeticion: CrearPeticion1Dto,
+    ): Promise<RespuestaPeticion1> {
+        const { Carrera, Materias } = datosPeticion;
+        return await this.universidadRepository.crearCarreraConMaterias({
+            Carrera,
+            Materias,
+        });
+    }
 
-  async crearPeticion2(
-    datosPeticion: CrearPeticion2Dto,
-  ): Promise<RespuestaPeticion2> {
-    const { Ciclo, Carrera, Estudiantes, Matricula } = datosPeticion;
+    async crearPeticion2(
+        datosPeticion: CrearPeticion2Dto,
+    ): Promise<RespuestaPeticion2> {
+        const { Ciclo, Carrera, Estudiantes, Matricula } = datosPeticion;
 
-    return await this.universidadRepository.crearCicloEstudiantesMatriculas({
-      Ciclo,
-      Carrera,
-      Estudiantes,
-      Matricula,
-    });
-  }
+        return await this.universidadRepository.crearCicloEstudiantesMatriculas({
+            Ciclo,
+            Carrera,
+            Estudiantes,
+            Matricula,
+        });
+    }
 
-  async crearPeticion3(
-    datosPeticion: CrearPeticion3Dto,
-  ): Promise<RespuestaPeticion3> {
-    const { Laboratorio, Ciclo, Matricula, Materia } = datosPeticion;
+    async crearPeticion3(
+        datosPeticion: CrearPeticion3Dto,
+    ): Promise<RespuestaPeticion3> {
+        const { Laboratorio, Ciclo, Matricula, Materia } = datosPeticion;
 
-    return await this.universidadRepository.asignarLaboratorio({
-      Laboratorio,
-      Ciclo,
-      Matricula,
-      Materia,
-    });
-  }
+        return await this.universidadRepository.asignarLaboratorio({
+            Laboratorio,
+            Ciclo,
+            Matricula,
+            Materia,
+        });
+    }
 }
